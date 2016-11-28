2c89bdd8ac1db3d73e3b15520d7ec44b26d386e0
zenlinlin <545963564@qq.com>
2016-11-28 14:09:44 +0800

    Update rref.R

--- rref.R
+++ rref.R
@@ -5,18 +5,21 @@ rref <- function(x) {
       n <- ncol(x)
       for (j in 1:n) {
         if (j > m) 
-          next else
+          next else {
             if (all(x[j:m, j] == 0)) 
-              next else
+              next else {
                 if (x[j, j] == 0) {
                   below <- x[(j+1):m, j]
                   Nonzero <- which(below != 0)[1] + j
                   x[j, ] <- x[Nonzero, ]
                   }
+                x[j, j:n] <- x[j, j:n] / x[j, j]
                 for (i in 1:m) {
                   if (i == j) next
                   x[i, j:n] <- x[i, j:n] - x[j, j:n] * x[i, j] / x[j, j]
                 }
+              }
+          } 
       }
       #substitute() returns the name of object
     } else cat("\nThe type of", " \"", substitute(x), "\" is not numeric.\n", sep="")
