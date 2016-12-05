742b748a9bca0f64cdfa8eb583e8a7fe1f33d5f8
zenlinlin <545963564@qq.com>
2016-12-05 11:27:22 +0800

    Update rref.R

--- rref.R
+++ rref.R
@@ -1,28 +1,26 @@
 rref <- function(x) {
-  if (is.matrix(x)) {
-    if (is.numeric(x)) {
-      m <- nrow(x)
-      n <- ncol(x)
-      for (j in 1:n) {
-        if (j > m) 
-          next else {
-            if (all(x[j:m, j] == 0)) 
-              next else {
-                if (x[j, j] == 0) {
-                  below <- x[(j+1):m, j]
-                  Nonzero <- which(below != 0)[1] + j
-                  x[j, ] <- x[Nonzero, ]
-                  }
-                x[j, j:n] <- x[j, j:n] / x[j, j]
-                for (i in 1:m) {
-                  if (i == j) next
-                  x[i, j:n] <- x[i, j:n] - x[j, j:n] * x[i, j] / x[j, j]
-                }
-              }
-          } 
-      }
-      #substitute() returns the name of object
-    } else cat("\nThe type of", " \"", substitute(x), "\" is not numeric.\n", sep="")
-  } else cat("\n\"", substitute(x), "\" is not a matrix.\n")
-  return(x)
+  if (!is.matrix(x))
+    stop("The type of", " \"", substitute(x), "\" is not numeric.", sep="")
+  if (!is.numeric(x))
+    stop("\"", substitute(x), "\" is not a matrix.")
+  m <- nrow(x)
+  n <- ncol(x)
+  for (j in 1:n) {
+    if (j > m)
+      next
+    if (all(x[j:m, j] == 0))
+      next
+    if (x[j, j] == 0) {
+      below <- x[(j+1):m, j]
+      Nonzero <- which(below != 0)[1] + j
+      x[j, ] <- x[Nonzero, ]
+    }
+    x[j, j:n] <- x[j, j:n] / x[j, j]
+    for (i in 1:m) {
+      if (i == j)
+        next
+      x[i, j:n] <- x[i, j:n] - x[j, j:n] * x[i, j] / x[j, j]
+    }
+  }
+  x
 }
